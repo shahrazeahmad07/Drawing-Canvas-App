4aa0d66045dbeb79059fdfb67efd46bf3a10faac
Shahzaib Ahmad <shahrazeahmad07@gmail.com>
2023-02-27 10:39:51 +0500

    Completed Drawing board, a custom view made with canvas.

---
+++ app/src/main/java/com/example/drawingcanvasapp/DrawingView.kt
@@ -0,0 +1,102 @@
+package com.example.drawingcanvasapp
+
+import android.app.Notification.Action
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+class DrawingView(context: Context, attr: AttributeSet) : View(context, attr) {
+    private lateinit var drawPaint: Paint
+    private lateinit var drawPath: CustomPath
+    private var color: Int = Color.BLACK
+    private var brushSize: Float = 0.toFloat()
+    private lateinit var canvasBitmap: Bitmap
+    private lateinit var canvas: Canvas
+    private lateinit var canvasPaint: Paint
+
+    private val paths = ArrayList<CustomPath>()
+
+    init {
+        setUpDrawing()
+    }
+
+    //! bta rahe k style kya ho ga draw kerne ka bus with using drawPaint variable.
+    private fun setUpDrawing() {
+        drawPaint = Paint()
+        drawPath = CustomPath(color, brushSize)
+        canvasPaint = Paint(Paint.DITHER_FLAG)
+        drawPaint.style = Paint.Style.STROKE
+        drawPaint.strokeJoin = Paint.Join.ROUND
+        drawPaint.strokeCap = Paint.Cap.ROUND
+        brushSize = 10.toFloat()
+    }
+
+    //! canvas set ker rahe bus bitmap create kerte apna or wo canvas main pas ker dete k ye hai hamara bitmap
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(canvasBitmap)
+    }
+
+    //! ye method draw kerwata, us paint ko jo hum ne uper design btaya jis ka, us path per jo hum
+    // touch event se la rahe onTouchEvent method main se
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?.drawBitmap(canvasBitmap, 0f, 0f, canvasPaint)
+
+        for (path in paths) {
+            drawPaint.strokeWidth = path.brushThickness
+            drawPaint.color = path.color
+            canvas?.drawPath(path, drawPaint)
+        }
+
+        drawPaint.strokeWidth = drawPath.brushThickness
+        drawPaint.color = drawPath.color
+        canvas?.drawPath(drawPath, drawPaint)
+    }
+
+    //! identify ker raha k kis kis path per touch kya user nai or wo path ko mention ker raha bus
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when(event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                drawPath.color = color
+                drawPath.brushThickness = brushSize
+                drawPath.reset()
+                if (touchX != null) {
+                    if (touchY != null) {
+                        drawPath.moveTo(touchX, touchY)
+                    }
+                }
+            }
+            MotionEvent.ACTION_MOVE -> {
+                if (touchX != null) {
+                    if (touchY != null) {
+                        drawPath.lineTo(touchX, touchY)
+                    }
+                }
+            }
+            MotionEvent.ACTION_UP -> {
+                paths.add(drawPath)
+                drawPath = CustomPath(color, brushSize)
+            }
+            else -> return false
+        }
+        invalidate()
+        return true
+    }
+
+
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
+
+    }
+
+}
